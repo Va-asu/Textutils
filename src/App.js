8d4df12b1f1fd60fa3d0a4d4294632ea6d711be8
vasudev <vsdvprmr@gmail.com>
2022-03-23 12:20:28 +0530

    added multiple themes palette

--- src/App.js
+++ src/App.js
@@ -25,30 +25,33 @@ function App() {
       }, 2000);
       //after 2 second alert will be dismissed automatically
    }
-  
-     const toggleMode=()=>{
-     if(mode==='light')
-     {
-        setMode('dark');
-        settxt('Enable light mode');
-        document.body.style.backgroundColor="#031632";//set document color to grey
-        showAlert("dark mode enabled","success");
-        //document.title="Textutil-darkmode"   this is how we can dynamicaly change our documents title
-        //porpose or this interval blinking is to attact attaintion from user
-        // setInterval(() => {
-        //   document.title="texutils is amazing"
-        // }, 2000);
-        // setInterval(() => {
-        //   document.title="install textutils"
-        // }, 1500);
-        
-     }else{
-        setMode('light');
-        settxt('Enable dark mode');
-        document.body.style.backgroundColor="white";
-        showAlert("light mode enabled","success");
+   const removeClasses=()=>{
+      document.body.classList.remove("bg-success");
+      document.body.classList.remove("bg-warning");
+      document.body.classList.remove("bg-primary");
+      document.body.classList.remove("bg-danger");
+      document.body.classList.remove("bg-dark");
 
-     }
+   }
+  
+    const toggleMode=(cls)=>{
+       removeClasses();
+       if(cls!=="dark"){
+         document.body.classList.add("bg-"+cls);
+       }
+       if(mode==='light')
+       {
+          setMode('dark');
+          settxt('Enable light mode');
+          document.body.style.backgroundColor="#031632";
+          showAlert("dark mode enabled","success");
+       }else{
+          setMode('light');
+          settxt('Enable dark mode');
+          document.body.style.backgroundColor="white";
+          showAlert("light mode enabled","success");
+  
+       }
    }
 
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,16 +23,13 @@ export default function Navbar(props) {
           <Link className="nav-link" to="/about">{props.about}</Link>
         </li>
       </ul>
-      {/* <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
-      {/* <button className="button mx-2" style={{"borderRadius": "70%",backgroundColor:"red",color:"red"}}>50</button> */}
-      
-      <div className={`form-check form-switch text-${props.mode==="light"?"dark":"light"}`}>
-          <input className="form-check-input"  onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.txt}</label>
-      </div>
+      <div className="class d-flex">
+        <div className="class bg-danger rounded mx-2" onClick={()=>props.toggleMode("danger")} style={{height:"30px",width:"30px"}}></div>
+        <div className="class bg-primary rounded mx-2" onClick={()=>props.toggleMode("primary")} style={{height:"30px",width:"30px"}}></div>
+        <div className="class bg-success rounded mx-2" onClick={()=>props.toggleMode("success")} style={{height:"30px",width:"30px"}}></div>
+        <div className="class bg-warning rounded mx-2" onClick={()=>props.toggleMode("warning")} style={{height:"30px",width:"30px"}}></div>
+        <div className={`class bg-${props.mode==='light'?"dark":"light"} rounded mx-2`} onClick={()=>props.toggleMode("dark")} style={{height:"30px",width:"30px"}}></div>
+     </div>
     </div>
   </div>
 </nav>
